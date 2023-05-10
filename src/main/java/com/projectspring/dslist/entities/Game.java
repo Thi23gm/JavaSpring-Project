622bdc00c533a02c7c1b253c7f7fddd8099ef226
Thiago Gomes <thi23gm@gmail.com>
2023-05-10 15:45:38 -0300

    Game seed

--- src/main/java/com/projectspring/dslist/entities/Game.java
+++ src/main/java/com/projectspring/dslist/entities/Game.java
@@ -22,6 +22,7 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
 
     @Column(columnDefinition = "TEXT")
@@ -33,13 +34,14 @@ public class Game {
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -90,6 +92,15 @@ public class Game {
         this.platforms = platforms;
     }
 
+    // Score
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     // imgUrl
     public String getImgUrl() {
         return imgUrl;
@@ -117,6 +128,7 @@ public class Game {
         this.longDescription = longDescription;
     }
 
+    // equalsGame
     @Override
     public int hashCode() {
         return Objects.hash(id);
